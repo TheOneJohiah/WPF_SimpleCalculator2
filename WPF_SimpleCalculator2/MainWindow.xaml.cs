c3a3a8da188c49ca06a90ea9b8f77f6367067aef
Micah Thoreson <thoresonm1@gmail.com>
2019-02-15 17:56:24 -0500

    Finalized the project. Validation still needs added.

--- WPF_SimpleCalculator2/MainWindow.xaml.cs
+++ WPF_SimpleCalculator2/MainWindow.xaml.cs
@@ -20,9 +20,64 @@ namespace WPF_SimpleCalculator2
     /// </summary>
     public partial class MainWindow : Window
     {
+        private enum Distance { feet, meters};
+        private enum Mass { Pounds, Kilograms};
+
+        Distance _distance;
+        Mass _mass;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            double mass;
+            double distancePerSecond;
+            double velocity;
+
+            if ((bool)RadioButton_Pounds.IsChecked)
+            {
+                double.TryParse(TextBox_Mass.Text, out mass);
+                double.TryParse(TextBox_Velocity.Text, out distancePerSecond);
+                mass = mass * 0.453592;
+                distancePerSecond = distancePerSecond * 0.3048;
+            }
+            else
+            {
+                double.TryParse(TextBox_Mass.Text, out mass);
+                double.TryParse(TextBox_Velocity.Text, out distancePerSecond);
+            }
+
+            velocity = mass * distancePerSecond;
+            TextBox_Momentum.Text = $"{velocity.ToString()} m/s";
+
+        }
+
+        private void RadioButton_Units_Checked(object sender, RoutedEventArgs e)
+        {
+            if (IsLoaded)
+            {
+                UpdateUnits();
+            }
+        }
+
+        private void UpdateUnits()
+        {
+            if ((bool)RadioButton_Pounds.IsChecked)
+            {
+                _distance = Distance.feet;
+                _mass = Mass.Pounds;
+            }
+            else if ((bool)RadioButton_Kilograms.IsChecked)
+            {
+                _distance = Distance.meters;
+                _mass = Mass.Kilograms;
+            }
+
+            Label_Velocity.Content = $"Velocity ({_distance}/s)";
+            Label_Mass.Content = _mass;
         }
     }
 }
